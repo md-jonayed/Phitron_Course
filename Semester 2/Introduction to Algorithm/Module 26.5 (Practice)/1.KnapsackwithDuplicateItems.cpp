60f0b8969cfe40d9f1edce9521d748efe4595720
Jonayed <jonayed@Jonayeds-MacBook-Pro.local>
2023-09-11 23:32:06 +0600

    practice done

---
+++ Semester 2/Introduction to Algorithm/Module 26.5 (Practice)/1.KnapsackwithDuplicateItems.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    int w;
+    cin >> n >> w;
+    int val[n], weight[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> val[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cin >> weight[i];
+    }
+    int dp[n + 1][w + 1];
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <= w; j++)
+        {
+            if (i == 0 || j == 0)
+            {
+                dp[i][j] = 0;
+            }
+        }
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= w; j++)
+        {
+            if (weight[i - 1] <= j)
+            {
+                dp[i][j] = max(dp[i][j - weight[i - 1]] + val[i - 1], dp[i - 1][j]);
+            }
+            else
+            {
+                dp[i][j] = dp[i - 1][j];
+            }
+        }
+    }
+    cout << dp[n][w] << endl;
+    return 0;
+}
