9c4cd82fe6abb5c166cb71e93ad4744e804e0c4f
Jonayed <jonayed@Jonayeds-MacBook-Pro.local>
2025-05-29 00:01:29 +0600

    module 10 html creation in app and project folder

---
+++ Semester 3/3. Software Development Project/Module 10 (Working with Django Template)/Project2/project_2/first_app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+
+def home(request):
+    # return HttpResponse("<h1> Home page </h1>")
+    return render(request,'first_app/home.html')
