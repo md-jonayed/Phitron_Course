f028c7a0322b5ebb567e3123e76664733452d763
Jonayed <jonayed@Jonayeds-MacBook-Pro.local>
2025-06-19 01:05:55 +0600

    done

---
+++ Semester 3/3. Software Development Project/Module 15 (Simple Blog Project Part 1)/Blogs/posts/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from categories.models import Category
+from authors.models import Author
+# Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    category = models.ManyToManyField(Category,)
+# one post can be in multiple categories and one category have multiple posts
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    # this means if a author's profile is deleted then all the posts under his profile will be deleted'
+    # one author can have multiple posts
+
+    def __str__(self):
+        return f"{self.title} -> {self.author}"
