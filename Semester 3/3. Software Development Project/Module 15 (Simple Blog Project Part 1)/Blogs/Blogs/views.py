6b9c42416191cc60a9190e5d84c97875ecd72b0a
Jonayed <jonayed@Jonayeds-MacBook-Pro.local>
2025-07-09 01:06:20 +0600

    done

---
+++ Semester 3/3. Software Development Project/Module 15 (Simple Blog Project Part 1)/Blogs/Blogs/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, redirect
+from posts.models import Post
+
+
+def home(request):
+    data = Post.objects.all()
+    # print(data)
+    return render(request, 'home.html', context={'data': data})
