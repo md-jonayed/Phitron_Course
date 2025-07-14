3aa1498d51b34c66ba5e057b4aafbad2ac0122da
Jonayed <jonayed@Jonayeds-MacBook-Pro.local>
2025-07-14 14:09:01 +0600

    module 15 done

--- Semester 3/3. Software Development Project/Module 15 (Simple Blog Project Part 1)/Blogs/posts/urls.py
+++ Semester 3/3. Software Development Project/Module 15 (Simple Blog Project Part 1)/Blogs/posts/urls.py
@@ -4,4 +4,6 @@ from .import views
 
 urlpatterns = [
     path('add/', views.add_posts, name="add_posts"),
+    path('edit/<int:id>', views.edit_posts, name="edit_posts"),
+    path('delete/<int:id>', views.delete_posts, name="delete_posts"),
 ]
--- Semester 3/3. Software Development Project/Module 15 (Simple Blog Project Part 1)/Blogs/posts/views.py
+++ Semester 3/3. Software Development Project/Module 15 (Simple Blog Project Part 1)/Blogs/posts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from .import forms
+from .import models
+from django.contrib import messages
 # Create your views here.
 
 
@@ -12,3 +14,23 @@ def add_posts(request):
     else:
         postForm = forms.PostForm()
     return render(request, 'post.html', context={'form': postForm})
+
+
+def edit_posts(request, id):
+    post = models.Post.objects.get(pk=id)
+    postForm = forms.PostForm(instance=post)
+    if request.method == 'POST':
+        postForm = forms.PostForm(request.POST, instance=post)
+        if postForm.is_valid():
+            postForm.save()
+            return redirect('home')
+    # else:
+    #     postForm = forms.PostForm()
+    return render(request, 'post.html', context={'form': postForm})
+
+
+def delete_posts(request, id):
+    post = models.Post.objects.get(pk=id)
+    post.delete()
+    messages.warning(request, 'The post has been deleted.')
+    return redirect('home')
